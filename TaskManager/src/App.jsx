9a20542c03a5fe4a49a1f235b1d091fc9da2f853
Barbacosta <christianacosta95.ca@gmail.com>
2025-03-01 20:30:21 -0600

    added outside click handling to dropdown

--- TaskManager/src/App.jsx
+++ TaskManager/src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css"
 import Dropdown from "./components/Dropdown/Dropdown"
+import DropdownItem from "./components/DropdownItem/DropdownItem";
 
 const App = () => {
-    const items = [1,2,3,4,5,6,7,8];
+    const items = [1,2,3,4,5,6,7,8,9,0];
 
     return(
         <div className="App">
-            <div className="content"><Dropdown buttonText="Task List" content={<p>Hello World</p>}/>
+            <div className="content">
+                <Dropdown buttonText="Task List" content={
+                    <>
+                        {items.map((item) => (
+                            <DropdownItem key={item}>
+                                {`Item ${item}`}
+                            </DropdownItem>
+                        ))}
+                    </>
+                }/>
             </div>
         </div>
     );
--- TaskManager/src/components/Dropdown/Dropdown.jsx
+++ TaskManager/src/components/Dropdown/Dropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import "./Dropdown.css";
 import DropdownButton from '../DropdownButton/DropdownButton'
@@ -7,14 +7,34 @@ import DropdownContent from '../DropdownContent/DropdownContent'
 const Dropdown = ({buttonText, content}) => {
     const [open, setOpen] = useState(false);
 
+    const dropdownRef = useRef();
+
     const toggleDropdown = () => {
       setOpen((open) => !open);
     };
 
+
+    useEffect(() => {
+      const handler = (event) => {
+        if(dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+          setOpen(false);
+        }
+      };  
+
+        document.addEventListener("click", handler);
+
+        return () => {
+          document.removeEventListener("click", handler);
+        }
+    }, []);
+
+
   return (
-    <div className="dropdown"><DropdownButton toggle={toggleDropdown} open={open}>
-      {buttonText}
-      </DropdownButton><DropdownContent open={open}>{content}</DropdownContent></div>
+    <div className="dropdown" ref={dropdownRef}>
+      <DropdownButton toggle={toggleDropdown} open={open}>
+        {buttonText}
+      </DropdownButton><DropdownContent open={open}>{content}</DropdownContent>
+    </div>
   )
 }
 
